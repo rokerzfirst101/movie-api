d80ae431c1d7c9157aad2da362b45a9685027715
rokerzfirst101 <rakshit.lko@gmail.com>
2022-11-21 05:00:50 +0530

    added mux

--- main.go
+++ main.go
@@ -2,24 +2,33 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"html"
-	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"strings"
+	"strconv"
 
 	"demoapi/models"
+
+	"github.com/gorilla/mux"
 )
 
-var movieList map[string]models.Movie
+var movieList map[int]models.Movie
 
 func main() {
-	movieList = make(map[string]models.Movie)
+	movieList = make(map[int]models.Movie)
 
-	http.HandleFunc("/movies", CreateMovie)
-	http.HandleFunc("/movies/", MovieHandler)
-	http.ListenAndServe(":9000", nil)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", CreateMovie).Methods(http.MethodPost)
+	r.HandleFunc("/movies/{id}", GetById).Methods(http.MethodGet)
+	r.HandleFunc("/movies/{id}", UpdateById).Methods(http.MethodPut)
+	r.HandleFunc("/movies/{id}", DeleteById).Methods(http.MethodDelete)
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:9000",
+	}
+	log.Fatalln(srv.ListenAndServe())
 }
 
 func CreateMovie(w http.ResponseWriter, r *http.Request) {
@@ -28,70 +37,109 @@ func CreateMovie(w http.ResponseWriter, r *http.Request) {
 	var movie models.Movie
 
 	if r.Method != http.MethodPost {
-		http.Error(w, "Bad Request", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
 	body, readErr := ioutil.ReadAll(r.Body)
 	if readErr != nil {
-		http.Error(w, "Can't Read Body", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
 	parseErr := json.Unmarshal(body, &movie)
 	if parseErr != nil {
-		fmt.Print(parseErr)
-		http.Error(w, "Invalid Body:", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	_, isOk := movieList[movie.ID]
 	if isOk {
-		http.Error(w, "Movie Already Exists", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
 	movieList[movie.ID] = movie
 	val, err := json.Marshal(movie)
 	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
 	}
-	io.WriteString(w, string(val[:]))
+	w.Write(val)
 }
 
-func MovieHandler(w http.ResponseWriter, r *http.Request) {
+func GetById(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	param := mux.Vars(r)
+	idParam := param["id"]
+
+	id, err := strconv.Atoi(idParam)
+	if err != nil || id <= 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	value, ok := movieList[id]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(value)
+}
+
+func UpdateById(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 
-	id := strings.Split(r.URL.String(), "/")[2]
+	param := mux.Vars(r)
+	idParam := param["id"]
 
-	switch r.Method {
-	case http.MethodGet:
-		value, ok := movieList[id]
-		if !ok {
-			http.Error(w, "Movie does not exist.", http.StatusBadRequest)
-			return
-		}
-		json.NewEncoder(w).Encode(value)
-	case http.MethodDelete:
-		_, ok := movieList[id]
-		if !ok {
-			fmt.Fprintf(w, "GET %q", html.EscapeString(r.URL.Path))
-			return
-		}
-		delete(movieList, id)
-	case http.MethodPut:
-		var movie models.Movie
-		body, readErr := ioutil.ReadAll(r.Body)
-		if readErr != nil {
-			http.Error(w, "Can't Read Body", http.StatusBadRequest)
-			return
-		}
-
-		parseErr := json.Unmarshal(body, &movie)
-		if parseErr != nil {
-			fmt.Print(parseErr)
-			http.Error(w, "Invalid Body:", http.StatusBadRequest)
-			return
-		}
-		movieList[id] = movie
+	id, err := strconv.Atoi(idParam)
+	if err != nil || id <= 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
+
+	_, exists := movieList[id]
+	if !exists {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	var movie models.Movie
+
+	body, readErr := ioutil.ReadAll(r.Body)
+	if readErr != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	parseErr := json.Unmarshal(body, &movie)
+	if parseErr != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	movieList[id] = movie
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func DeleteById(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+
+	param := mux.Vars(r)
+	idParam := param["id"]
+
+	id, err := strconv.Atoi(idParam)
+	if err != nil || id <= 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	_, ok := movieList[id]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+	delete(movieList, id)
 }
--- models/movie.go
+++ models/movie.go
@@ -1,7 +1,7 @@
 package models
 
 type Movie struct {
-	ID       string  `json: "id"`
+	ID       int     `json: "id"`
 	Name     string  `json: "name"`
 	Genre    string  `json: "genre"`
 	Rating   float64 `json: "rating"`
